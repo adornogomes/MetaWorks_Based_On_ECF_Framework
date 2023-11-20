6dc81db29adc3958cc6a5c3c3724f9ee65d9a224
Daniel Adorno Gomes <32546263+adornogomes@users.noreply.github.com>
2023-11-20 01:07:27 +0000

    Add files via upload

---
+++ win_metaworks_ecf_main_script.ps1
@@ -0,0 +1,51 @@
+# This function verifies and install a package with Chocolatey
+function VerifyAndInstallPackage {
+    param(
+        [string]$package
+    )
+
+    if (-not (Get-Command $package -ErrorAction SilentlyContinue)) {
+        Write-Host "$package is not installed. Installing $package..."
+        choco install $package -y
+        
+        if ($?) {
+            Write-Host "$package was installed with success."
+        } else {
+            Write-Host "Error during the instalation of the $package. Verify the error messages returned."
+			break
+        }
+    } else {
+        Write-Host "$package is already installed."
+    }
+}
+
+# Verify and install Chocolatey
+if (-not (Test-Path 'C:\ProgramData\chocolatey\choco.exe')) {
+    Write-Host "Chocolatey is not installed. Starting the process of installation..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+    if ($?) {
+        Write-Host "Chocolatey was installed with success."
+    } else {
+        Write-Host "Error during the instalation of the Chocolatey. Verify the error messages returned."
+		break
+    }
+} else {
+    Write-Host "Chocolatey is already installed."
+}
+
+# Verify and install Vagrant
+VerifyAndInstallPackage 'vagrant'
+
+# Verify and install VirtualBox
+VerifyAndInstallPackage 'virtualbox'
+
+# Download the scripts from Github
+# Invoke-RestMethod -Uri https://raw.githubusercontent.com/adornogomes/MetaWorks_Based_On_ECF_Framework/main/Vagrantfile -OutFile Vagrantfile
+# Invoke-RestMethod -Uri https://raw.githubusercontent.com/adornogomes/MetaWorks_Based_On_ECF_Framework/main/metaworks_ecf.yml -OutFile metaworks_ecf.yml
+
+# Starting Vagrant
+vagrant up
+
+
+
